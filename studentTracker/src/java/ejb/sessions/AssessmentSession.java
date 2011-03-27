08822d0f2aebe31452925bddf9860a185eb8eece
Michael Morris <mm336@blue98.ex.ac.uk>
2011-03-27 12:50:46 +0100

    micmo:start of sunday

---
+++ studentTracker/src/java/ejb/sessions/AssessmentSession.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ejb.sessions;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author mm336
+ */
+@Stateless
+public class AssessmentSession implements AssessmentSessionRemote {
+    
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+ 
+}
