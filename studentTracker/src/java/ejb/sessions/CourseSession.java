e0a35c428ea335b7b8c1e17b884ccd47c37d85a1
hugh halford-thompson <hughht5@gmail.com>
2011-03-27 15:37:06 +0100

    mcmo

--- studentTracker/src/java/ejb/sessions/CourseSession.java
+++ studentTracker/src/java/ejb/sessions/CourseSession.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ejb.sessions;
 
 import ejb.entities.Course;
@@ -17,7 +16,11 @@ import javax.persistence.PersistenceContext;
 /**
  *
  * The Course session allows clients to add and remove Courses from the system
- * and to access details of a Course and a list of all available Courses.
+ * and to access details of a Course and a list of all available Courses. Users
+ * can also view a list of all users on a particular course.
+ *
+ * These methods are directly related to courses allowing users to access all
+ * course information in one  place.
  *
  */
 @Stateless
@@ -26,6 +29,12 @@ public class CourseSession implements CourseSessionRemote {
     @PersistenceContext
     EntityManager manager;
 
+    /**
+     * Method to add a new Course
+     * @param _courseID
+     * @param _name
+     * @return true for success, false otherwise
+     */
     @Override
     public boolean addCourse(String _courseID, String _name) {
         try {
@@ -39,9 +48,14 @@ public class CourseSession implements CourseSessionRemote {
         return true;
     }
 
+    /**
+     * Remove a course from the system
+     * @param _course
+     * @return true for success, false otherwise
+     */
     @Override
     public boolean removeCourse(Course _course) {
-        try{
+        try {
             manager.remove(manager.find(Course.class, _course.getId()));
         } catch (Exception e) {
             return false;
@@ -49,65 +63,77 @@ public class CourseSession implements CourseSessionRemote {
         return true;
     }
 
+    /**
+     * Get a Course object by it's ID
+     * @param _ID
+     * @return the course if found, null otherwise
+     */
     @Override
     public Course getCourseByID(String _ID) {
-         List<Course> courses;
-        try{
+        List<Course> courses;
+        try {
             String query = "SELECT course FROM Course as course";
 
             courses = manager.createQuery(query).getResultList();
 
-            for(Course c : courses)
-            {
-               if(c.getCourseID().equals(_ID))
-                   return c;
+            for (Course c : courses) {
+                if (c.getCourseID().equals(_ID)) {
+                    return c;
+                }
             }
             return null;
         } catch (Exception e) {
-            System.out.println("getCourseByID ERROR: "+e);
+            System.out.println("getCourseByID ERROR: " + e);
             return null;
         }
     }
 
+    /**
+     * Method to get a list of all Courses in the system
+     * @return a list of Courses
+     */
     @Override
     public List<Course> getListOfCourses() {
-         List<Course> courses;
-        try{
+        List<Course> courses;
+        try {
             String query = "SELECT course FROM Course as course";
-
             courses = manager.createQuery(query).getResultList();
             return courses;
         } catch (Exception e) {
-            System.out.println("getListOfCourses ERROR: "+e);
+            System.out.println("getListOfCourses ERROR: " + e);
             return null;
         }
     }
 
+    /**
+     * Method to get a list of all Students on a particular course
+     * @param _courseID
+     * @return The list of students. This list can be empty.
+     */
     @Override
     public Collection<Student> getListOfStudentsOnCourse(String _courseID) {
         Collection<Student> students;
         Collection<Student> studentsOnCourse = new ArrayList();
-        try{
+        try {
             String query = "SELECT student FROM Student as student";
 
             students = manager.createQuery(query).getResultList();
 
-            for(Student s : students)
-            {
-                if(s.getCourse() != null)
-                    if(s.getCourse().getCourseID().equals(_courseID))
+            for (Student s : students) {
+                if (s.getCourse() != null) {
+                    if (s.getCourse().getCourseID().equals(_courseID)) {
                         studentsOnCourse.add(s);
+                    }
+                }
             }
 
             return studentsOnCourse;
         } catch (Exception e) {
-            System.out.println("getListOfStudentsOnCourse ERROR: "+e);
+            System.out.println("getListOfStudentsOnCourse ERROR: " + e);
             return null;
         }
 
     }
-
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
- 
 }
