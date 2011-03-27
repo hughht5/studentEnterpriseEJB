463f5630035a37e234c27323cd8540587463da11
Michael Morris <mm336@blue98.ex.ac.uk>
2011-03-27 14:12:45 +0100

    micmo:after merging

---
+++ studentTracker/src/java/ejb/sessions/ModuleSession.java
@@ -0,0 +1,114 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.sessions;
+
+import ejb.entities.Course;
+import ejb.entities.CourseModules;
+import ejb.entities.Module;
+import ejb.entities.Prerequisites;
+import java.util.Collection;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author hmh205
+ */
+@Stateless
+public class ModuleSession implements ModuleSessionRemote {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    @PersistenceContext
+    EntityManager manager;
+
+    @Override
+    public boolean addModule(String moduleID, String name, int credits, float avgMark, String stage, Collection<Module> prerequisites) {
+        try {
+            Module module = new Module();
+            module.setModuleID(moduleID);
+            module.setStage(stage);
+            module.setName(name);
+            module.setCredits(credits);
+            module.setAvgMark(avgMark);
+            module.setListOfPrereqs(prerequisites);
+            manager.persist(module);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean removeModule(Module module) {
+        try {
+            manager.remove(manager.find(Module.class, module.getId()));
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addModuleToCourse(Module _module, Course _course, boolean _isCompulsary) {
+        try {
+            CourseModules courseModule = new CourseModules();
+            courseModule.setCourse(manager.find(Course.class, _course.getId()));
+            courseModule.setModule(manager.find(Module.class, _module.getId()));
+            courseModule.setIsCompulsary(_isCompulsary);
+            manager.persist(courseModule);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public Module getModuleByID(String _ID) {
+         List<Module> modules;
+        try{
+            String query = "SELECT module FROM Module as module";
+
+            modules = manager.createQuery(query).getResultList();
+
+            for(Module m : modules)
+            {
+               if(m.getModuleID().equals(_ID))
+                   return m;
+            }
+            return null;
+        } catch (Exception e) {
+            System.out.println("getCourseByID ERROR: "+e);
+            return null;
+        }
+    }
+
+    @Override
+    public boolean removeModuleFromCourse(CourseModules _courseModule) {
+        try {
+            manager.remove(manager.find(CourseModules.class, _courseModule.getId()));
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public List<Module> getListOfAllModules() {
+         List<Module> modules;
+        try{
+            String query = "SELECT module FROM Module as module";
+
+            modules = manager.createQuery(query).getResultList();
+            return modules;
+        } catch (Exception e) {
+            System.out.println("getListOfAllModules ERROR: "+e);
+            return null;
+        }
+    }
+
+}
---
+++ studentTracker/src/java/ejb/sessions/ModuleSessionRemote.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.sessions;
+
+import ejb.entities.Course;
+import ejb.entities.CourseModules;
+import ejb.entities.Module;
+import ejb.entities.Prerequisites;
+import java.util.Collection;
+import java.util.List;
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author hmh205
+ */
+@Remote
+public interface ModuleSessionRemote {
+
+    boolean addModule(String moduleID, String name, int credits, float AVGMark, String stage, Collection<Module> prerequisites);
+
+    boolean removeModule(Module module);
+
+    boolean addModuleToCourse(Module _module, Course _course, boolean _isCompulsary);
+
+    Module getModuleByID(String _ID);
+
+    boolean removeModuleFromCourse(CourseModules _courseModule);
+
+    List<Module> getListOfAllModules();
+}
